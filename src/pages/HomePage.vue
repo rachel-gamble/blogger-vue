f4e016a3c5b9fdcdc167e28595a6ca65494e3269
Kayla Cammack <kaylacammack@gmail.com>
2022-12-08 16:06:52 -0700

    Formatted template on home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-
-      <!-- <div v-for="b in blogs" class="col-12 col-md-3 p-4"> -->
-
-      <div class="col-md-10 text-center rounded p-2 d-flex">
-
-        <img src="https://thiscatdoesnotexist.com" class="img-fluid rounded-circle elevated-2 p-2 profile-pic">
-        <h5 class="mt-4">author</h5>
-      </div>
-      <div>
-        <b class="fs-3">blog title</b>
-
-      </div>
-      <br>
-
-      <!-- <h5>{{blog.title}}</h5>
-            <h6></h6> -->
-
-
-      <!-- </div> -->
-
-
-
+    <div class="container-fluid ">
+        <div class="row">
+            <div class="col-md-10 text-center rounded p-2 d-flex">
+                <img src="https://thiscatdoesnotexist.com" class="img-fluid rounded-circle elevated-2 p-2 profile-pic">
+                <h5 class="mt-4">author</h5>
+            </div>
+            <div>
+                <b class="fs-3">blog title</b>
+            </div>
+            <br>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
